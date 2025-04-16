86eb5beb1b9110edd56dd4a96a9761d26da34ec8
yuran1811 <trieuvanbd123@gmail.com>
2025-04-16 14:54:40 +0700

    feat: ✨ update api

--- server/api/latest-notes.ts
+++ server/api/latest-notes.ts
@@ -6,13 +6,13 @@ const querySchema = z.object({
 });
 
 export default defineEventHandler(async (ev) => {
-  const res = await queryCollection(ev, 'blog').order('date', 'DESC').all();
+  const res = await queryCollection(ev, 'notes').order('date', 'DESC').all();
 
   const { limit, page } = await getValidatedQuery(ev, querySchema.parse);
   const reqLimit = limit ? Number.parseInt(limit.replace(/\D*/g, '')) : 0;
   const reqPage = page ? Number.parseInt(page.replace(/\D*/g, '')) : 0;
 
-  const allPosts = res.map((_) => ({
+  const allNotes = res.map((_) => ({
     id: _.id,
     title: _.title,
     date: _.date,
@@ -20,6 +20,6 @@ export default defineEventHandler(async (ev) => {
     tags: _.tags,
   }));
 
-  if (!page || !reqLimit || reqLimit > res.length) return allPosts;
-  return allPosts.slice(reqPage * reqLimit, (reqPage + 1) * reqLimit);
+  if (!page || !reqLimit || reqLimit > res.length) return allNotes;
+  return allNotes.slice(reqPage * reqLimit, (reqPage + 1) * reqLimit);
 });
