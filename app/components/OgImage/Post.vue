2bb06750b80906bc61a603964521c7f64be2e75e
yuran1811 <trieuvanbd123@gmail.com>
2025-04-17 12:08:46 +0700

    feat: ✨ add OgImage components

---
+++ app/components/OgImage/Post.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    title?: string;
+    borderColor?: string;
+  }>(),
+  {
+    title: 'title',
+    borderColor: 'blue-500',
+  },
+);
+</script>
+
+<template>
+  <div
+    :class="[`border-${borderColor}`]"
+    class="flex h-full w-full items-start justify-start border-[12px] border-solid bg-gray-50"
+  >
+    <div class="flex h-full items-start justify-start">
+      <div class="flex h-full w-full flex-col justify-between">
+        <h1 class="p-20 text-left text-[80px] font-black">
+          {{ title }}
+        </h1>
+        <p class="mb-0 px-20 pb-10 text-2xl font-bold">mycoolsite.com</p>
+      </div>
+    </div>
+  </div>
+</template>
