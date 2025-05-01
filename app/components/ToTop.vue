62f67363c0a024e5cdb2b60ab1537d9d03a61f78
yuran1811 <trieuvanbd123@gmail.com>
2025-05-02 01:57:17 +0700

    feat: ✨ enhance ui

--- app/components/ToTop.vue
+++ app/components/ToTop.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import { useWindowScroll } from '@vueuse/core';
 
+const isTooLow = useMediaQuery('(max-height: 220px)');
 const { y } = useWindowScroll({ behavior: 'smooth' });
 </script>
 
 <template>
-  <div class="totop group" :class="{ hidden: y < 350 }" @click="y = 0">
+  <div
+    class="totop group"
+    :class="{ hidden: y < 350 || isTooLow }"
+    @click="y = 0"
+  >
     <span
       class="absolute right-0 bottom-0 left-0 text-center text-xl font-bold text-transparent transition-all duration-200 group-hover:bottom-4 group-hover:text-current"
     >
